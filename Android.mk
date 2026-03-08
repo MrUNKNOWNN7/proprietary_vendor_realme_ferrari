effc7c4a625b0ec3e46303c7dd85cf6fbe658541
asmodeus-oos <mohamed20020093@gmail.com>
2026-03-08 00:34:35 +0000

    Track proprietary .so files with Git LFS v2

--- Android.mk
+++ Android.mk
@@ -3,3 +3,32 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),ferrari)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,3948ed9fe893c91cdd6d276ff10401d0dccc5bfb)
+$(call add-radio-file-sha1-checked,radio/aop.img,a4bfa1b8240e3aa1cc8b480fb2787b166d45de12)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,3bd26af8c0fb9d4ac2738934bdd647e56fb4b9d5)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,da2fc6cf316f4a5f5c9f65c37d6ac1c4cc922bfe)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,830060e7dea7cba3c79b373d74fb0c6835c203fb)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,0d07ef18bc18bf9cef278da1b4057c75821250a7)
+$(call add-radio-file-sha1-checked,radio/dsp.img,e9f84bc1929d77e5bcf2c8b184bfaed6c7942afb)
+$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,c7d125ec5e63232111908de845dce5f4c043d3e9)
+$(call add-radio-file-sha1-checked,radio/hyp.img,d0ed1208e7eae2d90dd695bdb6861987286c5d31)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,d9decc99b72992b28c67b31fb3eb44894ef7e5d2)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,6e55c30011bde6d11698a755dd5438a40ac9e6bd)
+$(call add-radio-file-sha1-checked,radio/modem.img,d141bf4e0abccf4d75f7c53de1c7bd5211a1fd08)
+$(call add-radio-file-sha1-checked,radio/oplus_sec.img,903e1685b64a523d629ac41fce6191b472b8a6e0)
+$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,482e4cf34a05bca3bd9d7ad42debb39cbf1d5c48)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,e8d5d467875b926d7962fa0a01a5bb41770f3751)
+$(call add-radio-file-sha1-checked,radio/shrm.img,ed0368bd3c30c1140b97c122497e3adaffab0af9)
+$(call add-radio-file-sha1-checked,radio/splash.img,88b02f550aa70d1d51f8197ee83eb026c49f28e2)
+$(call add-radio-file-sha1-checked,radio/tz.img,e27c6c7d6b5a1670864dbc2e8f33361c71ad9042)
+$(call add-radio-file-sha1-checked,radio/uefi.img,888b1af83d6fc5b3d3b891c3a2c03482744457a9)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,2a33a205e23cc9ab02117d0ba738a07716d05225)
+$(call add-radio-file-sha1-checked,radio/xbl.img,403d4a5466fb0635867dbbfee1c0e94d4fdcd71b)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,0e3cde7cb18cc5435279e7b4237adbc4035ce470)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,56be08c09658faf08e75d57e63202aa88a42c0b1)
+
+endif
